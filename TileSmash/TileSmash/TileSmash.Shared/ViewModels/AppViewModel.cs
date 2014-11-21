86d8f931a0c0ee7e1988d4c373ac274947e14ce4
NikolaBozhkov <n.bozhkov.ads@gmail.com>
2014-11-21 09:46:42 +0200

    Game Page UI finished

--- TileSmash/TileSmash/TileSmash.Shared/ViewModels/AppViewModel.cs
+++ TileSmash/TileSmash/TileSmash.Shared/ViewModels/AppViewModel.cs
@@ -8,34 +8,40 @@
     using Windows.UI.Xaml.Controls;
     using Windows.UI.Xaml.Media;
 
-    public class AppViewModel: ViewModelBase
+    public class AppViewModel : ViewModelBase
     {
         private static Random random = new Random();
 
+        private static Color[] colors = new Color[] 
+        {
+            Color.FromArgb(255, 251, 160, 38), // Neon Carrot
+            Color.FromArgb(255, 225, 73, 56), // Well Read
+            Color.FromArgb(255, 65, 168, 95), // Chateau Green
+            Color.FromArgb(255, 41, 105, 176), // Denim
+            Color.FromArgb(255, 250, 197, 28) // Turbo
+        };
+
         public AppViewModel()
         {
-            this.Hud = new HudViewModel(0);
+            var randomColorBrush = new SolidColorBrush(this.GetRandomColor());
+            this.GameModel = new GameViewModel(0, randomColorBrush);
         }
 
-        public HudViewModel Hud { get; set; }
+        public GameViewModel GameModel { get; set; }
+
+        private Color GetRandomColor()
+        {
+            return colors[random.Next(0, colors.Length)];
+        }
 
         public void InitializeGameGrid(Grid gameGrid)
         {
-            var colors = new Color[] 
-            {
-                Color.FromArgb(255, 251, 160, 38), // Neon Carrot
-                Color.FromArgb(255, 225, 73, 56), // Well Read
-                Color.FromArgb(255, 65, 168, 95), // Chateau Green
-                Color.FromArgb(255, 41, 105, 176), // Denim
-                Color.FromArgb(255, 250, 197, 28) // Turbo
-            };
-
             for (int i = 0; i < gameGrid.RowDefinitions.Count; i++)
             {
                 for (int j = 0; j < gameGrid.ColumnDefinitions.Count; j++)
                 {
                     var border = new Border();
-                    border.Background = new SolidColorBrush(colors[random.Next(0, colors.Length)]);
+                    border.Background = new SolidColorBrush(this.GetRandomColor());
                     border.Margin = new Thickness(4.5);
                     border.CornerRadius = new CornerRadius(4);
                     border.BorderThickness = new Thickness(1);
---
+++ TileSmash/TileSmash/TileSmash.Shared/ViewModels/GameViewModel.cs
@@ -0,0 +1,50 @@
+﻿namespace TileSmash.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Windows.UI.Xaml.Media;
+
+    public class GameViewModel
+    {
+        public const int MaxStonesInField = 10;
+        public const int SecondsToDestroy = 5;
+
+        private int[] colorBlockCounts;
+
+        public GameViewModel(int best, SolidColorBrush currentColor)
+        {
+            this.Best = best;
+            this.CurrentColor = currentColor;
+            this.Score = 0;
+            this.Stones = 0;
+            this.TimeLeft = new TimeSpan(0, 0, SecondsToDestroy);
+        }
+
+        public int MaxStones
+        {
+            get
+            {
+                return MaxStonesInField;
+            }
+        }
+
+        public int Stones { get; set; }
+
+        public int Score { get; set; }
+
+        public int Best { get; set; }
+
+        public TimeSpan TimeLeft { get; set; }
+
+        public string TimeLeftFormatted
+        {
+            get
+            {
+                return string.Format("{0:s\\.ff}", this.TimeLeft);
+            }
+        }
+
+        public SolidColorBrush CurrentColor { get; set; }
+    }
+}
